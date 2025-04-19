67783532e87064ff8ebb02a608eeb7618f62b7a6
sergiyshakhov <sergey.shakhov@gmail.com>
2025-04-19 22:03:36 +0200

    Convert to class

--- js/main.js
+++ js/main.js
@@ -1,32 +1,28 @@
-const Tix = function () {
-    function Tix() {
-        const _this = this;
+class Tix {
+    constructor() {
         this.hoursTensElement = document.querySelector('#hoursTens');
         this.hoursUnitElement = document.querySelector('#hoursUnits');
         this.minutesTensElement = document.querySelector('#minutesTens');
         this.minutesUnitElement = document.querySelector('#minutesUnits');
 
-        setInterval(function () {
-            return _this.showTime();
-        }, 5000);
-
+        setInterval(() => this.showTime(), 5000);
         this.showTime();
     }
 
-    Tix.prototype.flashDigit = function (element, digit) {
+    flashDigit(element, digit) {
         const segmentsCount = getComputedStyle(element).getPropertyValue("--segments");
         const segments = this.randomSegments(segmentsCount, digit);
         this.flashElement(element, segments);
-    };
+    }
 
-    Tix.prototype.flashElement = function (element, segments) {
+    flashElement(element, segments) {
         const segmentElements = element.children;
         for (let i = 0, j = 1; i < segmentElements.length; i++, j *= 2) {
             segmentElements[i].style.backgroundColor = (segments & j) > 0 ? element.dataset.color : 'inherit';
         }
-    };
+    }
 
-    Tix.prototype.showTime = function () {
+    showTime() {
         const time = new Date();
         const minutesUnits = time.getMinutes() % 10;
         const minutesTens = (time.getMinutes() - minutesUnits) / 10;
@@ -36,22 +32,19 @@ const Tix = function () {
         this.flashDigit(this.hoursUnitElement, hoursUnits);
         this.flashDigit(this.minutesTensElement, minutesTens);
         this.flashDigit(this.minutesUnitElement, minutesUnits);
-    };
+    }
 
-    Tix.twoPower = [1, 2, 4, 8, 16, 32, 64, 128, 256];
+    static twoPower = [1, 2, 4, 8, 16, 32, 64, 128, 256];
 
-    Tix.prototype.randomSegments = function (capacity, digit) {
+    randomSegments(capacity, digit) {
         let result = 0;
         let segments = Tix.twoPower.slice(0, capacity);
         for (let i = 0; i < digit; i++) {
             const index = Math.floor(Math.random() * segments.length);
             result += segments.splice(index, 1)[0];
         }
-
         return result;
-    };
-
-    return Tix;
-}();
+    }
+}
 
 new Tix();
